510523d50d6a035d659c62bec763de47285b6d57
Dustin Hurtt <dustin_hurtt@outlook.com>
2023-10-16 16:24:50 -0400

    added routes

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
+var cors = require('cors')
 
 var usersRouter = require('./routes/users');
 var projectsRouter = require('./routes/projects')
@@ -15,9 +17,23 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(
+    cors({
+      origin: ['http://localhost:5173']  // <== URL of our future React app
+    })
+  );
 
 app.use('/users', usersRouter);
 app.use('/projects', projectsRouter);
-app.use('/tasks', tasksRouter)
+app.use('/tasks', tasksRouter);
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then((x) => {
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
+  })
+  .catch((err) => {
+    console.error("Error connecting to mongo: ", err);
+  });
 
 module.exports = app;
--- routes/tasks.js
+++ routes/tasks.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
 
+const Task = require('../models/Task')
+const Project = require('../models/Project')
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
+router.post("/", (req, res, next) => {
+  const { title, description, projectId } = req.body;
+ 
+  Task.create({ title, description, project: projectId })
+    .then((newTask) => {
+      return Project.findByIdAndUpdate(
+        projectId, 
+        { $push: { tasks: newTask._id } },
+        { new: true} )
+    })
+    .then((response) => {
+      return response.populate('tasks')
+    })
+    .then((response) => {
+      res.json(response)
+    })
+    .catch(err => res.json(err));
 });
 
 module.exports = router;
